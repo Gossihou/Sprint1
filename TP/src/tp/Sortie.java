8490ce259b8b9bb2ef979475bb30e6f38aa54b00
Gossihou <ossihoug2000@gmail.com>
2024-05-31 23:00:05 -0400

    Update Sortie.java

--- TP/src/tp/Sortie.java
+++ TP/src/tp/Sortie.java
@@ -14,6 +14,28 @@ public class Sortie {
         
 JSONObject terrainObject = new JSONObject();
 
+<<<<<<< HEAD
+=======
+ sortie.accumulate("valeur_fonciere_totale", terrainObject.getvaleurFronciere());
+ sortie.accumulate("taxe_scolaire",terrainObject.gettaxeMunicipale());
+ sortie.accumulate("taxe_municipale", terrainObject.getTaxeMunicipale());
+ 
+ JSONArray lotissements = new JSONArray();
+ 
+ for (Lot lot : terrainObject.getLotissements()) {
+             JSONObject lotissement = new JSONObject();
+            lotissement.add("description", lot.getDescription());
+            lotissement.add("valeur_par_lot", lot.getValeurParLot());
+            lotissements.add(lotissement);
+        }
+sortie.accumulate("lotissements", lotissements);
+
+ try { //Le bloc de code lève l'exception au cas où l'écriture serait erronée.
+            FileWriter.saveStringIntoFile(filepath, sortie.toString()); //Utilise la méthode du Filewriter.
+        } catch (IOException e) {
+            System.err.println("Erreur lors de l'enregistrement du fichier de sortie: " + e.getMessage());
+        }
+>>>>>>> parent of 4f3d35f (deuxième commit-incrementation de la classe Sortie pour l'affichage de sortie du logiciel demander par le client.)
 }
     
 }
