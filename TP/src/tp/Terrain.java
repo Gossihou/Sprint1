0ad98cd17091b2d09176a11b27a2071577ad974a
Gossihou <ossihoug2000@gmail.com>
2024-06-01 21:55:34 -0400

    troisième commit: Création de la classe Terain qui contient toute les constances fournie pour le logiciel.

---
+++ TP/src/tp/Terrain.java
@@ -0,0 +1,11 @@
+
+package tp;
+
+/**
+ *Classe terrain contenant les constantes neccesaire au clauls faite dans 
+ * classe calculator
+ * @author gloireossihou
+ */
+public class Terrain {
+    
+}
